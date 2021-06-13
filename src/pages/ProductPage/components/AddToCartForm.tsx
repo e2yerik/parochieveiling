06da42dbd773f423a5afbeb0c0f060aff4e8f2ab
Erik Slagter <erik.slagter@e2ycommerce.com>
2021-06-13 15:13:32 +0200

    remove 0 bid

--- src/pages/ProductPage/components/AddToCartForm.tsx
+++ src/pages/ProductPage/components/AddToCartForm.tsx
@@ -108,7 +108,9 @@ const AddtoCartForm: React.FC<AddToCartFormProps> = (
   }
 
   const singleStep = product.step || 10;
-  const steps = [0, 1, 2, 3, 4, 5].map((i: number) => singleStep * i);
+  const steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+    (i: number) => singleStep * i
+  );
 
   return (
     <>
